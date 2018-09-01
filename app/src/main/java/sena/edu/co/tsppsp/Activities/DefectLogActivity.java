8568a82d49ebe1a13dbea11dbe431f4e8316e35a
DistritoCapital2 <distritocapitalmovil2018@gmail.com>
2018-09-01 10:56:50 -0500

    chore: se sube layout defect log.

--- app/src/main/java/sena/edu/co/tsppsp/Activities/DefectLogActivity.java
+++ app/src/main/java/sena/edu/co/tsppsp/Activities/DefectLogActivity.java
@@ -3,13 +3,17 @@ package sena.edu.co.tsppsp.Activities;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 
 import sena.edu.co.tsppsp.R;
 
 public class DefectLogActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +26,12 @@ public class DefectLogActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+
             }
         });
     }
+
+
 
 }

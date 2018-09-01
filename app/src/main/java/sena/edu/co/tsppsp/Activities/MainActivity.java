04808d5268961d22a9089749c3daaae710830568
Distrito Capital <wskdistritocapital2018@gmail.com>
2018-09-01 10:59:46 -0500

    chore: Se agrega fuente

--- app/src/main/java/sena/edu/co/tsppsp/Activities/MainActivity.java
+++ app/src/main/java/sena/edu/co/tsppsp/Activities/MainActivity.java
@@ -1,20 +1,13 @@
 package sena.edu.co.tsppsp.Activities;
 
-import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import sena.edu.co.tsppsp.R;
@@ -46,38 +39,6 @@ public class MainActivity extends AppCompatActivity {
 
     private void abrirActivityRegisto() {
         startActivity(new Intent(this, ProjectRegisterActivity.class));
-    }
-
-
-    @Nullable
-    @Override
-    protected Dialog onCreateDialog(int id, Bundle args) {
-        return super.onCreateDialog(id, args);
-    }
-
-    // Metodo encargado de abrir AlertDialog de Registro de Nuevo Proyectos
-    private void abrirAlertRegistroNuevoProyecto() {
-
-        AlertDialog.Builder alertRegistro = new AlertDialog.Builder(getApplicationContext());
-        
-        View view = View.inflate(this,R.layout.alert_dialog_registo_nuevo_proyecto,null);
-
-        EditText    et_alert_d_nombre_proyecto = view.findViewById(R.id.et_alert_d_nombre_proyecto);
-        Button      btn_alert_d_registrar_proyecto = view.findViewById(R.id.btn_alert_d_registrar_proyecto);
-        
-        btn_alert_d_registrar_proyecto.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),"Registrado",Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        alertRegistro.setView(view);
-
-        AlertDialog dialog = alertRegistro.create();
-        dialog.show();
-
-        // TODO: Terminar logica AlertDialog
     }
 
     @Override
--- app/src/main/java/sena/edu/co/tsppsp/Activities/TimeLogActivity.java
+++ app/src/main/java/sena/edu/co/tsppsp/Activities/TimeLogActivity.java
@@ -1,15 +1,20 @@
 package sena.edu.co.tsppsp.Activities;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 import sena.edu.co.tsppsp.R;
 
 public class TimeLogActivity extends AppCompatActivity {
+
+    TextView tvTitleTL;
+    Typeface typeface;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +22,13 @@ public class TimeLogActivity extends AppCompatActivity {
         setContentView(R.layout.activity_time_log);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        tvTitleTL = findViewById(R.id.tvTitleTL);
+
+        typeface = Typeface.createFromAsset(getAssets(),"fonts/poorinc.tff");
+
+        tvTitleTL.setTypeface(typeface);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
